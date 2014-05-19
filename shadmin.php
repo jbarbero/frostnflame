59ca7a6e539955dd0f72d17f8fa6ef8314472351
jbarbero <jbarbero@gmail.com>
2014-05-19 01:49:06 -0700

    new files

---
+++ shadmin.php
@@ -0,0 +1,22 @@
+<?php
+include("header.php");
+
+template_display('admin.html');
+
+if ($users[num] != 1)
+    TheEnd("You are not the root administrator!");
+
+session_start();
+
+$_SESSION['root'] = 1;
+$_SESSION['mysql']['host'] = $config['dbhost'];
+$_SESSION['mysql']['user'] = $config['dbuser'];
+$_SESSION['mysql']['pass'] = $config['dbpass'];
+$_SESSION['mysql']['db'] = $config['dbname'];
+
+define('self', str_replace("&amp;", "&", "?admin$authstr"));
+include("minimyadmin.php");
+
+TheEnd("");
+
+?>
