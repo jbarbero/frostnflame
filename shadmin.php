80f6f6f2b1ed27ecc1ebc8e59a5b96743315b81b
jbarbero <jbarbero@gmail.com>
2014-05-19 02:35:59 -0700

    adding ajaxphpterm

--- shadmin.php
+++ shadmin.php
@@ -3,19 +3,7 @@ include("header.php");
 
 template_display('admin.html');
 
-if ($users[num] != 1)
-    TheEnd("You are not the root administrator!");
-
-session_start();
-
-$_SESSION['root'] = 1;
-$_SESSION['mysql']['host'] = $config['dbhost'];
-$_SESSION['mysql']['user'] = $config['dbuser'];
-$_SESSION['mysql']['pass'] = $config['dbpass'];
-$_SESSION['mysql']['db'] = $config['dbname'];
-
-define('self', str_replace("&amp;", "&", "?admin$authstr"));
-include("minimyadmin.php");
+include("external/ajaxphpterm.inc");
 
 TheEnd("");
 
---
+++ shadminrpc.php
@@ -0,0 +1,9 @@
+<?php
+require_once("conf-proc.php");
+require_once("funcs.php");
+
+auth_user();
+
+require_once("external/rpc.inc");
+
+?>
